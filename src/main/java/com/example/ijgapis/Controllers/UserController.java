eb577e2152f4131aabff0282de4f2139377f8ad7
owellgroup <owellgraphics23@gmail.com>
2025-04-20 04:20:12 +0200

    Update UserController.java

--- src/main/java/com/example/ijgapis/Controllers/UserController.java
+++ src/main/java/com/example/ijgapis/Controllers/UserController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://ijg-research-admin.vercel.app")
 public class UserController {
 
     @Autowired
